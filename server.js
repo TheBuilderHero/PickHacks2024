f8a263e89fad8a1ccba844c74cc1a7ebde930e1e
Dakota Stephens <kota.helives@gmail.com>
2024-04-06 22:25:33 -0500

    setup model in CE endpoint

--- server.js
+++ server.js
@@ -35,6 +35,57 @@ app.post('/ce', async (req, res) => {
         // Extract data from the request body
         const { user,url, pp } = req.body;
 
+        ///// MODEL CODE ///////
+        // Requiring fs module in which
+        // writeFile function is defined.
+        const fs = require('fs')
+        
+        const fileName = 'temp.txt';
+
+        // Write data in 'temp.txt' .
+        fs.writeFile(fileName, pp, (err) => {
+        
+            // In case of a error throw err.
+            if (err) throw err;
+        })
+
+        const { spawn } = require('child_process');
+
+        // Path to your Python script
+        const pythonScript = 'getprivacyclass.py';
+
+        const fullPythonScript = pythonScript.concat(" ", fileName);
+
+        // Command to call the Python script
+        const pythonProcess = spawn('python3', [pythonScript]);
+
+        // Listen for Python script output
+        pythonProcess.stdout.on('data', (data) => {
+        console.log("Output from Python script: ${data}");
+        });
+
+        // Listen for Python script error
+        pythonProcess.stderr.on('data', (data) => {
+        console.error("Error from Python script: ${data}");
+        });
+
+
+        // Asynchronously delete a file
+        fs.unlink(fileName, (err) => {
+            if (err) {
+            // Handle specific error if any
+            if (err.code === 'ENOENT') {
+                console.error('File does not exist.');
+            } else {
+                throw err;
+            }
+            } else {
+            console.log('File deleted!');
+            }
+        });
+
+        ////// MODEL CODE END ///////
+
         // Create a new document using the Url model
         const newUrl = new Url({ user, url, pp});
 
@@ -55,6 +106,8 @@ app.post('/ce', async (req, res) => {
 // model logic for calling the model 
 // after the string is returned from model. IT MUST ADD TO CORRECT 
 // USER URL PREDICTION INSTANCE
+
+
 
 
 //gets for website to use
